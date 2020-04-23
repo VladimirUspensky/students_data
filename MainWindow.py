e5301284151f88c3856f27284b1f3466e47cc293
Vladimir Uspensky <vl.shev@list.ru>
2020-04-23 20:26:24 +0300

    Change file tree

--- MainWindow.py
+++ MainWindow.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter import messagebox
 import os
-import copy
 
 
 class MainWindow:
@@ -43,11 +42,13 @@ class MainWindow:
         self.scroll_filesX.pack(side=BOTTOM, fill=X)
         self.CreateMenu()
 
-        self.tree = ttk.Treeview(self.tab1, yscrollcommand=self.scroll_filesY.set,
+        self.tree = ttk.Treeview(self.tab1, column=('first', 'second'), yscrollcommand=self.scroll_filesY.set,
                                  xscrollcommand=self.scroll_filesX.set)
+
 
         self.tree.configure(yscrollcommand=self.scroll_filesY.set, xscrollcommand=self.scroll_filesX.set)
         self.tree.place(relwidth=1, relheight=1)
+
 
         self.scroll_filesY.config(command=self.tree.yview)
         self.scroll_filesX.config(command=self.tree.xview)
@@ -57,10 +58,12 @@ class MainWindow:
         self.buttons_frame.place(relx=0.3, relwidth=0.2, relheight=0.25)
 
         self.all_files = {}
-        self.keys_counter = 0
+        self.delete_from_tree_btn = Button(self.buttons_frame, text='Delete file', relief=RAISED,
+                                        borderwidth=3, command=self.DeleteFileFromTree)
+        self.delete_from_tree_btn.place(relwidth=0.5, relheight=0.2)
         self.open_from_list_btn = Button(self.buttons_frame, text='Open file', relief=RAISED,
-                                         borderwidth=3, command=self.OpenFromTree)
-        self.open_from_list_btn.place(relwidth=1, relheight=0.2)
+                                        borderwidth=3, command=self.OpenFileFromTree)
+        self.open_from_list_btn.place(relx=0.5, relwidth=0.5, relheight=0.2)
 
         self.combobox_frame = Frame(self.root, relief=RAISED)
         self.combobox_frame.place(relx=0.5, relwidth=0.2, relheight=0.25)
@@ -68,7 +71,6 @@ class MainWindow:
         self.combobox = ttk.Combobox(self.combobox_frame)
         self.combobox['values'] = ('first', 'second', 'third')
         self.combobox.pack(fill=X)
-
         self.combo_btn = Button(self.combobox_frame, text='btn')
         self.combo_btn.pack(fill=X)
 
@@ -77,11 +79,40 @@ class MainWindow:
         self.lbl.pack()
         self.scale = ttk.Scale(self.combobox_frame, from_=0, to=100, command=self.OnScale)
         self.scale.pack(fill=X)
-        self.CreateDrawField()
 
     def OnScale(self, val):
         v = int(float(val))
         self.var.set(v)
+
+    def DeleteFileFromTree(self):
+        try:
+            selected_file = self.tree.focus()
+            parent = self.tree.parent(selected_file)
+            parent_idx = self.tree.index(parent)
+            file_index = self.tree.index(selected_file)
+            file_list = self.all_files.get(parent_idx)
+            file_list.pop(file_index)
+            self.all_files[parent_idx] = file_list
+
+            self.tree.delete(selected_file)
+            print(self.all_files)
+        except:
+            messagebox.showerror('Error', 'Choose the file')
+
+    def OpenFileFromTree(self):
+        try:
+            selected_file = self.tree.focus()
+            file_index = self.tree.index(selected_file)
+            parent = self.tree.parent(selected_file)
+            parent_idx = self.tree.index(parent)
+            file_list = self.all_files.get(parent_idx)
+            print(file_list[file_index])
+            opened_file = open(file_list[file_index], 'r')
+            self.txt_field.delete(0.0, END)
+            self.txt_field.insert(0.0, opened_file.read())
+            opened_file.close()
+        except AttributeError and TypeError:
+            messagebox.showerror('Error', 'choose the file')
 
     def CreateMenu(self):
         main_menu = Menu(self.root)
@@ -90,69 +121,41 @@ class MainWindow:
         open_files_menu = Menu(file_menu, tearoff=0)
         main_menu.add_cascade(label='File', menu=file_menu)
         file_menu.add_cascade(label='Add...', menu=open_files_menu)
-        open_files_menu.add_command(label='Add Project', command=self.Add)
+
+        open_files_menu.add_command(label='Add Folder', command=self.AddFolder)
+      #  open_files_menu.add_command(label='Add file', command=self.AddFile)
+
+    def AddFolder(self):
+        dir_path = fd.askdirectory()
+        file_tree = []
+        for file in os.walk(dir_path):
+            file_tree.append(file)
+        name = dir_path.split('/')
+        name.reverse()
+        dir_id = self.tree.insert('', END, name[0], text=name[0])
+
+        array = []
+        for address, dirs, files in file_tree:
+            for file in files:
+                self.tree.insert(dir_id, END, text=file)
+                array.append(address+'/'+file)
+        index = self.tree.index(dir_id)
+        self.all_files[index] = array
 
 
     def Run(self):
         self.root.mainloop()
 
-    def Add(self):
-        flag = False
-        flag_to_avoid_repeat = False
-        idx = 1
-        file_list = []
-        dir_path = fd.askdirectory()
-        parent_name = dir_path.split('/')
-        self.tree.insert('', 0, f'{parent_name[-1]}', text=parent_name[-1])
-        for file in os.walk(dir_path):
-            parent_name = file[0].split('/')[-1]
-            for dir in file[1]:
-                self.tree.insert(f'{parent_name}', idx, f'{parent_name}' + f'\{dir}', text=dir)
-                idx += 1
-                flag = True
-                if not flag_to_avoid_repeat:
-                    for fl in file[2]:
-                        self.tree.insert(f'{parent_name}', idx, text=fl)
-                        file_list.append(file[0] + '/' + fl)
-                        idx += 1
-                        flag_to_avoid_repeat = True
-                    self.all_files[f'{parent_name}'] = copy.deepcopy(file_list)
-                    file_list.clear()
-
-            if not flag:
-                for fl in file[2]:
-                    self.tree.insert(f'{parent_name}', idx, text=fl)
-                    file_list.append(file[0] + '/' + fl)
-                    idx += 1
-                flag = False
-                self.all_files[f'{parent_name}'] = copy.deepcopy(file_list)
-                file_list.clear()
-            flag_to_avoid_repeat = False
-
-
-    def OpenFromTree(self):
-        self.txt_field.delete(0.0, END)
-        selected_file = self.tree.focus()
-        try:
-            file_list = self.all_files[self.tree.parent(selected_file)]
-        except KeyError:
-            messagebox.showerror('Error', 'Import Project')
-        else:
-            file_index = self.tree.index(selected_file)
-            print(file_index)
-            file_path = file_list[file_index - 1]
-            file_path = open(file_path)
-            self.txt_field.insert(0.0, file_path.read())
-            file_path.close()
-
-    def CreateDrawField(self):
-        canvas_first_type = Canvas(self.tab2, bg='grey', cursor='pencil')
-        canvas_second_type = Canvas(self.tab2, bg='grey', cursor='pencil')
-        canvas_first_type.place(relwidth=1, relheight=0.5)
-        canvas_second_type.place(rely=0.5, relwidth=1, relheight=0.5)
-
-
-
+    """
+        def AddFile(self):
+            file = []
+            file_path = fd.askopenfilename()
+            file_name = file_path.split('/')
+            self.tree.insert('', END, text=file_name[len(file_name) - 1])
+            file.append(file_path)
+            self.all_files[len(self.all_files.keys())] = file
+            print(self.all_files)
+    """
 
 if __name__ == '__main__':
     window = MainWindow(600, 400)
